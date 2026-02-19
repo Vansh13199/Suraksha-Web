69d1704b0b8566b4a05ae9d169b36b80571d6ea3
“Vansh13199” <vansh25bcs80031@gmail.com>
2026-02-19 22:31:49 +0530

    web

---
+++ src/aws-exports.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+// WARNING: DO NOT EDIT. This file is manually constructed from amplifyconfiguration.dart
+
+const awsmobile = {
+    "aws_project_region": "eu-central-1",
+    "aws_appsync_graphqlEndpoint": "https://cw6p3n777rbzhdkeqtbfhrfu3y.appsync-api.eu-central-1.amazonaws.com/graphql",
+    "aws_appsync_region": "eu-central-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-ojtehu2kfncsbfmwt6ozqb7zhi",
+    "aws_cognito_identity_pool_id": "eu-central-1:921cce93-104f-4986-afea-503040b69aa9",
+    "aws_cognito_region": "eu-central-1",
+    "aws_user_pools_id": "eu-central-1_eDep25rpv",
+    "aws_user_pools_web_client_id": "3i0sm7d3q6nb9r9ln03i6ong0u",
+    "oauth": {},
+    "aws_cognito_username_attributes": [
+        "PHONE_NUMBER"
+    ],
+    "aws_cognito_social_providers": [],
+    "aws_cognito_signup_attributes": [
+        "EMAIL",
+        "PHONE_NUMBER"
+    ],
+    "aws_cognito_mfa_configuration": "OFF",
+    "aws_cognito_mfa_types": [
+        "SMS"
+    ],
+    "aws_cognito_password_protection_settings": {
+        "passwordPolicyMinLength": 8,
+        "passwordPolicyCharacters": []
+    },
+    "aws_cognito_verification_mechanisms": [
+        "EMAIL"
+    ]
+};
+
+export default awsmobile;
